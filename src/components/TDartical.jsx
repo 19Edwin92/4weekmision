b09fdc22f64a8c694c2a7522ce327616ec7bc361
19Edwin92 <b_a_s_e@naver.com>
2023-03-06 14:46:17 +0900

    Delete-modules

--- src/components/TDartical.jsx
+++ src/components/TDartical.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { TDarticalDiv } from './TDcss'
+import { useDispatch } from 'react-redux'
+import { D_EDLETETD } from '../modules/todolists';
 
 function TDartical() {
   const todolist = useSelector((state)=> {
     return state.todolist
-  })
+  });
+
+  const dispatch = useDispatch();
   
   return (
     <div>
@@ -15,12 +19,12 @@ function TDartical() {
           <p>{el.title}</p>
           <p>{el.txt}</p>
           <button>완료</button>
-          <button>삭제</button>
+          <button onClick={()=> dispatch(D_EDLETETD(el.id))}>삭제</button>
         </TDarticalDiv>
       )
     })}
     </div>
   )
-}
+};
 
-export default TDartical
+export default TDartical;
--- src/modules/todolists.js
+++ src/modules/todolists.js
@@ -1,16 +1,24 @@
 const initialState = [
   {id:1678075618574, title:"리액트 배우기(1)", txt:"리액트를 오늘부터 배워보자."},
+  {id:1678075618575, title:"리액트 배우기(2)", txt:"리액트를 오늘부터 배워보자."},
+  {id:1678075618576, title:"리액트 배우기(3)", txt:"리액트를 오늘부터 배워보자."},
 ]
 
 const PLUSETD = "pluse_todo"
+const EDLETETD = "delete_todo"
 
 export const D_PLUSETD = (payload) => {
-  console.log(payload.title)
-  console.log(payload.txt)
-  console.log(initialState)
   return {
     type:PLUSETD,
-    payload : payload
+    payload
+  }
+}
+
+export const D_EDLETETD = (payload) => {
+  console.log(payload)
+  return {
+    type:EDLETETD,
+    payload
   }
 }
 
@@ -19,6 +27,10 @@ const todolist = (state=initialState, action) => {
   switch (action.type) {
     case PLUSETD :
       return [...state, {id:Date.now(), title:action.payload.title, txt:action.payload.txt},]
+    case EDLETETD:
+      const deleteTD = state.filter(el=> el.id !== Number(action.payload))
+      console.log(deleteTD)
+      return state
     default :
       return state
   }
